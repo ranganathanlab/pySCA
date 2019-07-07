de7808d62e7cc7fce50380570bfc52bfed535c12
sudorook <daemon@nullcodon.com>
2019-07-07 13:47:56 -0500

    remove whitespace; pipe stderr ot stdout

--- runAllNBCalcs.sh
+++ runAllNBCalcs.sh
@@ -1,41 +1,52 @@
 #! /bin/bash
 set -eu
 
-# The S1A serine proteases 
-echo "S1A serine protease Calculations:" > Outputs/s1A_halabi.log
-./scaProcessMSA.py Inputs/s1Ahalabi_1470_nosnakes.an -s 3TGI -c E -t -n >> Outputs/s1A_halabi.log 
-./scaCore.py Outputs/s1Ahalabi_1470_nosnakes.db >> Outputs/s1A_halabi.log 
-./scaSectorID.py Outputs/s1Ahalabi_1470_nosnakes.db >> Outputs/s1A_halabi.log 
+mkdir -p output
 
-# Beta-lactamase 
-echo "Beta-lactamase Calculations:" > Outputs/PF13354.log
-./scaProcessMSA.py Inputs/PF13354_full.an -s 1FQG -c A -f 'Escherichia coli' -t -n >> Outputs/PF13354.log
-./scaCore.py Outputs/PF13354_full.db >> Outputs/PF13354.log
-./scaSectorID.py Outputs/PF13354_full.db >> Outputs/PF13354.log
+# The S1A serine proteases
+# echo "S1A serine protease Calculations:" 2>&1 > output/s1A_halabi.log
+echo "S1A serine protease Calculations:" > output/s1A_halabi.log 2>&1
+./scripts/scaProcessMSA.py data/s1Ahalabi_1470_nosnakes.an -s 3TGI -c E -t -n \
+  >> output/s1A_halabi.log 2>&1
+./scripts/scaCore.py output/s1Ahalabi_1470_nosnakes.db \
+  >> output/s1A_halabi.log 2>&1
+./scripts/scaSectorID.py output/s1Ahalabi_1470_nosnakes.db \
+  >> output/s1A_halabi.log 2>&1
 
-# G-protein - this analysis is run with two alignments - the full PFAM 
-# alignment (PF00071_full) and the PFAM alignment filtered to remove several 
+# Beta-lactamase
+echo "Beta-lactamase Calculations:" > output/PF13354.log
+./scripts/scaProcessMSA.py data/PF13354_full.an -s 1FQG -c A \
+  -f 'Escherichia coli' -t -n >> output/PF13354.log 2>&1
+./scripts/scaCore.py output/PF13354_full.db >> output/PF13354.log 2>&1
+./scripts/scaSectorID.py output/PF13354_full.db >> output/PF13354.log 2>&1
+
+# G-protein - this analysis is run with two alignments - the full PFAM
+# alignment (PF00071_full) and the PFAM alignment filtered to remove several
 # N-terminal truncation mutants. PF00071_rd2 is the aligment discussed in the
 # manuscript.
-echo "G-protein calculations:" > Outputs/PF00071.log
-./scaProcessMSA.py Inputs/PF00071_full.an -s 5P21 -c A -f 'Homo sapiens' -t -n >> Outputs/PF00071.log
-./scaCore.py Outputs/PF00071_full.db >> Outputs/PF00071.log
-./scaSectorID.py Outputs/PF00071_full.db >> Outputs/PF00071.log
+echo "G-protein calculations:" > output/PF00071.log
+./scripts/scaProcessMSA.py data/PF00071_full.an -s 5P21 -c A \
+  -f 'Homo sapiens' -t -n >> output/PF00071.log 2>&1
+./scripts/scaCore.py output/PF00071_full.db >> output/PF00071.log 2>&1
+./scripts/scaSectorID.py output/PF00071_full.db >> output/PF00071.log 2>&1
 
-echo "G-protein calculations:" > Outputs/PF00071_rd2.log
-./scaProcessMSA.py Inputs/PF00071_rd2.an -s 5P21 -c A -f 'Homo sapiens' -t -n >> Outputs/PF00071_rd2.log
-./scaCore.py Outputs/PF00071_rd2.db >> Outputs/PF00071_rd2.log
-./scaSectorID.py Outputs/PF00071_rd2.db >> Outputs/PF00071_rd2.log
+echo "G-protein calculations:" > output/PF00071_rd2.log
+./scripts/scaProcessMSA.py data/PF00071_rd2.an -s 5P21 -c A \
+  -f 'Homo sapiens' -t -n >> output/PF00071_rd2.log 2>&1
+./scripts/scaCore.py output/PF00071_rd2.db >> output/PF00071_rd2.log 2>&1
+./scripts/scaSectorID.py output/PF00071_rd2.db >> output/PF00071_rd2.log 2>&1
 
-# DHFR - this analysis is also run with two alignments for comparison - 
-# the full PFAM alignment (PF00186_full.an) and a manually curated alignment 
-# (DHFR_PEPM3.an)  
-echo "DHFR Calculations:" > Outputs/PF00186.log
-./scaProcessMSA.py Inputs/PF00186_full.an -s 1RX2 -c A -f 'Escherichia coli' -t -n >> Outputs/PF00186.log
-./scaCore.py Outputs/PF00186_full.db >> Outputs/PF00186.log
-./scaSectorID.py Outputs/PF00186_full.db >> Outputs/PF00186.log
+# DHFR - this analysis is also run with two alignments for comparison -
+# the full PFAM alignment (PF00186_full.an) and a manually curated alignment
+# (DHFR_PEPM3.an)
+echo "DHFR Calculations:" > output/PF00186.log
+./scripts/scaProcessMSA.py data/PF00186_full.an -s 1RX2 -c A \
+  -f 'Escherichia coli' -t -n >> output/PF00186.log 2>&1
+./scripts/scaCore.py output/PF00186_full.db >> output/PF00186.log 2>&1
+./scripts/scaSectorID.py output/PF00186_full.db >> output/PF00186.log 2>&1
 
-echo "DHFR Calculations:" > Outputs/DHFR_PEPM3.log
-./scaProcessMSA.py Inputs/DHFR_PEPM3.an -s 1RX2 -c A -t -n >> Outputs/DHFR_PEPM3.log
-./scaCore.py Outputs/DHFR_PEPM3.db >> Outputs/DHFR_PEPM3.log
-./scaSectorID.py Outputs/DHFR_PEPM3.db >> Outputs/DHFR_PEPM3.log
+echo "DHFR Calculations:" > output/DHFR_PEPM3.log
+./scripts/scaProcessMSA.py data/DHFR_PEPM3.an -s 1RX2 -c A -t -n \
+  >> output/DHFR_PEPM3.log 2>&1
+./scripts/scaCore.py output/DHFR_PEPM3.db >> output/DHFR_PEPM3.log 2>&1
+./scripts/scaSectorID.py output/DHFR_PEPM3.db >> output/DHFR_PEPM3.log 2>&1
