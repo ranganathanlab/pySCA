43a6d4110f7ffc88225c971446987651ea03e6b4
sudorook <daemon@nullcodon.com>
2019-07-07 19:52:21 -0500

    add note explaining the global path variables

--- pysca/settings.py
+++ pysca/settings.py
@@ -1,6 +1,10 @@
 ##########################################################################
 # PATHS
-# These have to be changed to be consistent with user-defined paths.
+#
+# These have to be changed to be consistent with user-defined paths. This
+# script is tested against the `runAllNBCalcs.sh` scripts, and because the
+# script includes a `cd ../` command before running any of the python scripts,
+# the base directory is the root of the repository.
 
 # (this directory should contain the file 'pfamseq.txt' from
 # ftp://ftp.sanger.ac.uk/pub/databases/Pfam/current_release/database_files/
