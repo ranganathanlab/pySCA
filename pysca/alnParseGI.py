4547bbce5853e1302cbf32dce947a00725b079fc
sudorook <daemon@nullcodon.com>
2019-10-07 16:49:50 -0500

    allow header delimiter to be specified in alnParseGI

--- pysca/alnParseGI.py
+++ pysca/alnParseGI.py
@@ -25,12 +25,14 @@ if __name__ == '__main__':
     # Parse inputs
     parser = argparse.ArgumentParser()
     parser.add_argument("alignment", help='Input Sequence Alignment')
-    parser.add_argument("--output", dest="outputfile", default='GI_Num',
+    parser.add_argument("-o", "--output", dest="outputfile", default='GI_Num',
                         help="specify an outputfile name")
+    parser.add_argument("-d", "--delim", dest="delim", default="_",
+                        help="specify the field delimiter in the header")
     options = parser.parse_args()
 
     headers, seqs = sca.readAlg(options.alignment)
-    gis = [h.split('_')[1] for h in headers]
+    gis = [h.split(options.delim)[1] for h in headers]
 
     f = open(options.outputfile, 'w')
     for gi in gis:
