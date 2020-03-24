ab46b90d3abb1c839abf852f6e435121abb34340
sudorook <daemon@nullcodon.com>
2020-03-23 21:44:56 -0500

    add jupyterlab as dependency

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     url="https://ranganathanlab.gitlab.io/pySCA",
     download_url="https://github.com/ranganathanlab/pySCA",
     long_description=open("README.md", "r").read(),
-    install_requires=["biopython", "numpy", "scipy", "argparse", "wheel", "matplotlib"],
+    install_requires=["biopython", "numpy", "scipy", "argparse", "wheel", "matplotlib", "jupyterlab"],
     scripts=[
         "bin/alnChangeDelim",
         "bin/alnFilterSeqSize",
